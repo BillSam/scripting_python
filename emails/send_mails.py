e362fb7763ee065b223b6b77da13d5288eb6fdb1
mbugua <email@email.com>
2021-02-17 14:19:28 +0300

    email_sender

--- emails/send_mails.py
+++ emails/send_mails.py
@@ -1,13 +1,17 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Testing'
 email['to'] = 'getelvest@gmail.com'
 email['subject'] = 'Yu won '
 
-email.set_content('I am a python master')
+# email.set_content('I am a python master')
 
+email.set_content(html.substitute(name='Tin Tin'), 'html')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
